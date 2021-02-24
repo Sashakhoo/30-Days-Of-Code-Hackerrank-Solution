35b8b822837eb3d5c4976d5c542fd60d22bb26ef
Natasya Khoo <sashakhoo8@gmail.com>
2021-02-24 23:43:39 +0800

    Add files via upload

---
+++ Day7Arrays.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day7Arrays {
+private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" "); // 1432 is input as a string 
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+
+        for (int i = n-1; i>=0; i--) {
+            int arrItem = Integer.parseInt(arrItems[i]); // passing string values into int form
+            arr[i] = arrItem;
+            System.out.print(arr[i]+" ");
+        }
+
+        scanner.close();
+    }
+}
